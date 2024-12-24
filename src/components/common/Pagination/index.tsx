179071d62150512ad8fa60c87365c2fcf72b05ca
Marvin Ronquillo <mondejarmarron18@gmail.com>
2024-12-24 23:32:49 +0800

    refactor:pagination link

--- src/components/common/Pagination/index.tsx
+++ src/components/common/Pagination/index.tsx
@@ -4,6 +4,7 @@ import React, { HTMLAttributes } from "react";
 import cn from "@/utils/cn";
 import { usePathname } from "next/navigation";
 import pages from "@/constants/pages";
+import Link from "next/link";
 
 type PaginationPropsType = HTMLAttributes<HTMLDivElement>;
 
@@ -16,13 +17,16 @@ const Pagination = ({ className, ...props }: PaginationPropsType) => {
       {...props}
     >
       {pages.map((page) => (
-        <div
+        <Link
+          href={page.url}
           key={page.name}
           className={cn("h-4 w-4 rounded-full", {
             "bg-customDirtyWhite dark:bg-customGray": path !== page.url,
             "bg-secondary dark:bg-primary": path === page.url,
           })}
-        ></div>
+        >
+          <span className="sr-only">{page.name}</span>
+        </Link>
       ))}
     </div>
   );
