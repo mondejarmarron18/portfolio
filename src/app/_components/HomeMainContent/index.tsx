11ac3fed08378e6fe075c87ed0f81200b7556e36
Marvin Ronquillo <marvin.ronquillo@tinvio.com>
2025-05-16 22:47:01 +0800

    refactor:homepage text

--- src/app/_components/HomeMainContent/index.tsx
+++ src/app/_components/HomeMainContent/index.tsx
@@ -8,6 +8,8 @@ import TypewriterComponent from "typewriter-effect";
 import Paragraph from "@/components/ui/Paragraph";
 import HomeCTAs from "../HomeCTAs";
 
+const roles = ["Software Engineer", "AI Integration Developer", "Web Designer"];
+
 const HomeMainContent = () => {
   return (
     <div className="scrollbar-thin flex h-full items-center overflow-y-auto">
@@ -23,13 +25,13 @@ const HomeMainContent = () => {
         <div className="flex flex-col gap-2">
           <Heading
             variant="h1"
-            className="flex h-fit flex-col sm:flex-row sm:gap-2 md:gap-4"
+            className="flex h-fit flex-col sm:flex-row sm:gap-2 md:gap-2"
           >
-            <span>{"Hi, I am Marvin a"}</span>
+            <span>{"Hi, I am Marvin"}</span>
             <span className="bg-gradient-to-tr from-primary to-secondary bg-clip-text text-transparent">
               <TypewriterComponent
                 options={{
-                  strings: ["Software Engineer", "UI/UX Designer"],
+                  strings: roles.map((role) => `| ${role}`),
                   autoStart: true,
                   loop: true,
                   delay: "natural",
@@ -38,13 +40,22 @@ const HomeMainContent = () => {
             </span>
           </Heading>
           <Paragraph className="text-sm sm:text-base">
-            A driven and dedicated software engineer for more than{" "}
+            {/* A driven and dedicated software engineer for more than{" "}
             <span className="bg-gradient-to-tr from-primary to-secondary bg-clip-text font-semibold text-transparent">
               4 years,
             </span>{" "}
             with a knack for creating innovative, efficient solutions and
             committed to delivering high-quality software that meets user needs
-            and exceeds expectations.
+            and exceeds expectations. */}
+            {/* Got an idea? Let’s bring it to life. I build scalable, high-quality
+            web solutions that turn concepts into real, impactful
+            products—efficiently and with attention to detail. */}
+            Got an idea? Let’s bring it to life. With my{" "}
+            <span className="bg-gradient-to-tr from-primary to-secondary bg-clip-text font-semibold text-transparent">
+              4+ years
+            </span>{" "}
+            of experience, I build scalable, high-quality web solutions that
+            turn ideas into real, impactful products.
           </Paragraph>
         </div>
         <HomeCTAs />
--- src/components/ui/Heading/index.tsx
+++ src/components/ui/Heading/index.tsx
@@ -23,21 +23,21 @@ const Heading = forwardRef<HTMLHeadingElement, HeadingPropsType>(
     switch (variant) {
       case "h2":
         return (
-          <h2 {...attibutes("text-xl md:text-2xl lg:text-3xl")}>{children}</h2>
+          <h2 {...attibutes("text-lg md:text-xl lg:text-2xl")}>{children}</h2>
         );
       case "h3":
         return (
-          <h3 {...attibutes("text-lg md:text-xl lg:text-2xl")}>{children}</h3>
+          <h3 {...attibutes("text-md md:text-lg lg:text-xl")}>{children}</h3>
         );
       case "h4":
-        return <h4 {...attibutes("md:text-lg lg:text-xl")}>{children}</h4>;
+        return <h4 {...attibutes("md:text-md lg:text-lg")}>{children}</h4>;
       case "h5":
-        return <h5 {...attibutes("text-lg")}>{children}</h5>;
+        return <h5 {...attibutes("text-md")}>{children}</h5>;
       case "h6":
         return <h6 {...attibutes("text-base")}>{children}</h6>;
       default:
         return (
-          <h1 {...attibutes("text-2xl md:text-3xl lg:text-4xl")}>{children}</h1>
+          <h1 {...attibutes("text-xl md:text-2xl lg:text-3xl")}>{children}</h1>
         );
     }
   },
